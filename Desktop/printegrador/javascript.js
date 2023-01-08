6436bbb6a61188973f4461c9feafb0ddac600b28
Renato Paracone <renatoparacone5@gmail.com>
2023-01-08 03:15:44 -0300

    nuevos cambios

---
+++ Desktop/printegrador/javascript.js
@@ -0,0 +1,22 @@
+nombre = prompt ("Ingrese su nombre")
+
+for (let is = 0; is<5; is++)
+{
+    if ((nombre != "") && (nombre != Number(nombre)))
+    {alert("Bienvenido " + nombre);
+    break;
+        }
+    else{   
+        nombre = prompt("Ingrese su nombre, por favor"); 
+        }
+
+
+console.log("JavaScript")
+
+
+
+
+}
+
+
+
---
+++ Desktop/printegrador/productos.js
@@ -0,0 +1,24 @@
+productos = prompt("Escriba su mensaje")
+
+
+for(let i = 0; i<5; i++) 
+{if ((productos=="Darth Vader") || (productos== "darth vader"))
+    {alert("tenemos el producto");
+        break;}
+    else if((productos=="torre eiffel") || (productos== "Torre Eiffel"))
+        {alert("Tenemos el producto");
+        break}
+    else if ((productos=="llavero star wars") || (productos == "Llavero Star Wars")){
+        alert("Tenemos el producto");
+        break;}
+else{
+    alert("No tenemos el producto");
+    }
+productos = prompt("Escriba su mensaje")
+}
+
+/* Preguntar al profesor como hacer para que 1) mande la alerta si terminan todas las opciones
+2 como aplicar todo esto a un ecomerce. */
+
+// algoritmo con ciclo //
+
